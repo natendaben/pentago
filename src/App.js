d36368daf73e283f76bfa1a6b858b6481f2b6074
Elsa Roeber <elsa.roeber@colorado.edu>
2019-11-19 09:53:30 -0700

    hi i changed the title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          Hello0o0o0o0o0o0o0
         </p>
         <a
           className="App-link"
